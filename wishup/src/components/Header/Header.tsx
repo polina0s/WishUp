b666afb6f1962682ebc5a9f2550ed8890231619e
Polina <polina.osintseva000@gmail.com>
2025-04-24 12:59:38 +0400

    added text component

--- wishup/src/components/Header/Header.tsx
+++ wishup/src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Toolbar } from '@mui/material'
 import star from '../../../public/images/Star.png'
+import { Text } from '../Text'
+import styles from './Header.module.scss'
 
 export const Header = () => {
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography>WISHUP</Typography>
+    <AppBar className={styles.header}>
+      <Toolbar className={styles.toolbar}>
+        <Text
+          variant="juliusSansOneLG"
+          color="blue-300"
+          className={styles.logo}
+        >
+          WISHUP
+        </Text>
         <img src={star} />
       </Toolbar>
     </AppBar>
