f57a29bb8f018a071b80be1eabc5b0d219a3b91e
Polina <polina.osintseva000@gmail.com>
2025-04-24 11:57:04 +0400

    added mui

--- wishup/src/main.tsx
+++ wishup/src/main.tsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './main.scss'
 import { router } from './router'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './styles/theme'
+import { CssBaseline, StyledEngineProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </StyledEngineProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
