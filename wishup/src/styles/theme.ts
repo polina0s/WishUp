bcfe96e1fa8197850f34c83aa7b8ad1d77e3918c
Polina <polina.osintseva000@gmail.com>
2025-04-24 16:36:43 +0400

    added variable foe box-shadow

--- wishup/src/styles/theme.ts
+++ wishup/src/styles/theme.ts
@@ -1,3 +1,14 @@
 import { createTheme } from '@mui/material/styles'
 
-export const theme = createTheme({})
+export const theme = createTheme({
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          '--Paper-shadow': 'none',
+          boxShadow: 'none',
+        },
+      },
+    },
+  },
+})
